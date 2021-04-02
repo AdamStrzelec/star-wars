f80c59fbc3cb3a4d7e885f662baecfc6b33afdc9
adastr96 <adam19960@op.pl>
2021-04-02 16:06:18 +0200

    add infinite scroll

--- src/components/CharactersContainer/CharactersContainer.tsx
+++ src/components/CharactersContainer/CharactersContainer.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { connect } from 'react-redux';
 import { fetchCharacters as fetchCharactersAction } from '../../actions/actions';
 
@@ -19,7 +20,25 @@ const CharactersContainer: React.FC<Props> = ({ characters, hasNextPage, fetchCh
 
     return(
         <div>
-
+            <InfiniteScroll
+                  dataLength={characters.length}
+                  next={() => {
+                        setCurrentPage(currentPage+1)
+                        fetchCharacters(currentPage+1, characters)
+                    }}
+                  hasMore={hasNextPage}
+                  loader={<h4>Loading...</h4>}
+            >
+                {
+                    characters.map(character => 
+                        <div key={character.name}>
+                            <h1>Name: {character.name}</h1>
+                            <h2>Gender: {character.gender}</h2>
+                            <h2>Birth year: {character.birth_year}</h2>
+                        </div>
+                    )
+                }
+            </InfiniteScroll>
         </div>
     )
 }
